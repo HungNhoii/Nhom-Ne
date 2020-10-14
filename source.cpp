cb85005b86928b7129ef9f836050338056538ff7
btt <blackmagician122@gmail.com>
2020-10-14 17:00:18 +0700

    them Tong 2 so Random

--- source.cpp
+++ source.cpp
@@ -1,55 +1,51 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 int tong(int a, int b){
-	int tong=0;
-	tong=a+b;
+	int tong = 0;
+	tong = a + b;
 	return tong;
 }
 int hieu(int a, int b){
-	int hieu=0;
-	hieu=a-b;
+	int hieu = 0;
+	hieu = a - b;
 	return hieu;	
 }
 int tich(int a, int b)
 {
-	int tich=1;
-	tich=a*b;
+	int tich = 1;
+	tich = a * b;
 	return tich;
 }
 float thuong(int a, int b)
 {
-	float thuong=0;
-	if (b==0)
-		cout<<"Error";
+	float thuong = 0;
+	if (b == 0)
+		cout << "Error";
 	else
-		thuong=float(a/b);
+		thuong = float(a) / float(b);
 	return thuong;
+}
+int soRandom()
+{
+	int so;
+	for (int i = 0; i <= 100; i++)
+		so = rand() % 100;
+	return so;
 }
 int main()
 {
 	int a, b;
-	char phepToan;
+	int r1 = soRandom();
+	int r2 = soRandom();
 	cout << "Nhap 2 so nguyen: ";
 	cin >> a >> b;
-	cout << "Tinh gi? (+,-,*,/) ";
-	cin >> ch;
-	switch (phepToan)
-	{
-	case '+':
-		cout<<"Tong"<<a<<" + "<<b<<"la: "<<tong(a,b)<<endl;
-		break;
-	case '-':
-		cout<<"Hieu"<<a<<" - "<<b<<"la: "<<hieu(a,b)<<endl;
-		break;
-	case '*':
-		cout<<"Tich"<<a<<" * "<<b<<"la: "<<tich(a,b)<<endl;
-		break;
-	case '/':
-		cout<<"Thuong"<<a<<" / "<<b<<"la: "<<thuong(a,b)<<endl;
-		break;
-	default:
-		cout << "Chon sai phep toan";
-		break;
-	}
+	cout << a << " + " << b << " = " << tong(a,b) << endl;
+	cout << a << " - " << b << " = " << hieu(a, b) << endl;
+	cout << a << " * " << b << " = " << tich(a, b) << endl;
+	cout << a << " / " << b << " = " << thuong(a, b) << endl;
+	cout << "So ngau nhien 1: " << r1 <<endl;
+	cout << "So ngau nhien 2: " << r2 <<endl;
+	cout << "Tong 2 so ngau nhien " << r1 << " + " << r2 << " = " << tong(r1, r2) << endl;
 	return 0;
 }
