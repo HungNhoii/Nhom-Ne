7c34d9784aeb44cdddd234ae6bc1d6684283e2f6
Your Name <you@example.com>
2020-10-14 15:55:03 +0700

    first

--- source.cpp
+++ source.cpp
@@ -1,55 +1,47 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b){
-	int tong=0;
-	tong=a+b;
+int tong(int a, int b) {
+	int tong = 0;
+	tong = a + b;
 	return tong;
 }
-int hieu(int a, int b){
-	int hieu=0;
-	hieu=a-b;
-	return hieu;	
+int hieu(int a, int b) {
+	int hieu = 0;
+	hieu = a - b;
+	return hieu;
 }
 int tich(int a, int b)
 {
-	int tich=1;
-	tich=a*b;
+	int tich = 1;
+	tich = a * b;
 	return tich;
 }
 float thuong(int a, int b)
 {
-	float thuong=0;
-	if (b==0)
-		cout<<"Error";
+	float thuong = 0;
+	if (b == 0)
+		cout << "Error";
 	else
-		thuong=float(a/b);
+		thuong = float(a / b);
 	return thuong;
 }
+//float cvdtHV(float canh) {
+//	float chuvi, dientich;
+//	chuvi = canh * 4;
+//	dientich = canh * canh;
+//	cout << "chu vi hinh vuong:" << chuvi << endl << "dien tich hinh vuong:" << dientich;
+//}
+
+
 int main()
 {
 	int a, b;
-	char phepToan;
 	cout << "Nhap 2 so nguyen: ";
 	cin >> a >> b;
-	cout << "Tinh gi? (+,-,*,/) ";
-	cin >> ch;
-	switch (phepToan)
-	{
-	case '+':
-		cout<<"Tong"<<a<<" + "<<b<<"la: "<<tong(a,b)<<endl;
-		break;
-	case '-':
-		cout<<"Hieu"<<a<<" - "<<b<<"la: "<<hieu(a,b)<<endl;
-		break;
-	case '*':
-		cout<<"Tich"<<a<<" * "<<b<<"la: "<<tich(a,b)<<endl;
-		break;
-	case '/':
-		cout<<"Thuong"<<a<<" / "<<b<<"la: "<<thuong(a,b)<<endl;
-		break;
-	default:
-		cout << "Chon sai phep toan";
-		break;
-	}
+	cout << a << "+" << b << "=" << tong(a, b) << endl;
+	cout << a << "-" << b << "=" << hieu(a, b) << endl;
+	cout << a << "*" << b << "=" << tich(a, b) << endl;
+	cout << a << "/" << b << "=" << thuong(a, b) << endl;
+
 	return 0;
 }
