4b2d2cd6d7b4aa52c895776a7caba055686e2128
unknown <vu.hieuuu50@gmail.com>
2020-10-21 16:17:28 +0700

    update chu vi va dien tich lan 2

--- source.cpp
+++ source.cpp
@@ -15,7 +15,7 @@ float chuViHinhChuNhat(float dai, float rong)
 	return (dai+rong)*2;
 }
 
-float dienTichHinhVuong(float dai, float rong)
+float dienTichHinhChuNhat(float dai, float rong)
 {
 	return dai*rong;
 }
@@ -31,5 +31,32 @@ float dienTichHinhTron(float bankinh)
 }
 int main()
 {
+	int luachon;
+	cout<<"1.Tinh chu vi dien tich hinh vuong."<<endl;
+	cout<<"2.Tinh chu vi dien tich hinh chu nhat."<<endl;
+	cout<<"3.Tinh chu vi dien tich hinh tron."<<endl;
+	cout<<"Nhap lua chon: ";
+	cin>>luachon;
+	switch(luachon)
+	{
+		case 1:
+			int canh;
+			cout<<"Nhap chieu dai canh hinh vuong: ";
+			cin>>canh;
+			cout<<"Chu vi = "<<chuViHinhVuong(canh)<<"; Dien tich = "<<dienTichHinhVuong(canh);
+			break;
+		case 2:
+			int dai, rong;
+			cout<<" Nhap chieu dai 2 canh hinh chu nhat: ";
+			cin>>dai>>rong;
+			cout<<"Chu vi = "<<chuViHinhChuNhat(dai,rong)<<"; Dien tich = "<<dienTichHinhChuNhat(dai,rong);
+			break;
+		case 3:
+			int bankinh;
+			cout<<"Nhap chieu dai ban kinh hinh tron: ";
+			cin>>bankinh;
+			cout<<"Chu vi = "<<chuViHinhTron(bankinh)<<"; Dien tich = "<<dienTichHinhTron(bankinh);
+			break;
+	}
 	return 0;
 }
