8584db53c6ffe0277eefac84bc88f053175b15f7
unknown <vu.hieuuu50@gmail.com>
2020-10-21 16:05:52 +0700

    update chu vi va dien tich

--- source.cpp
+++ source.cpp
@@ -1,66 +1,35 @@
 #include <iostream>
 using namespace std;
-#define PI 3.14;
-int tong(int a, int b) {
-	int tong = 0;
-	tong = a + b;
-	return tong;
-}
-int hieu(int a, int b) {
-	int hieu = 0;
-	hieu = a - b;
-	return hieu;
-}
-int tich(int a, int b)
+
+float chuViHinhVuong(float canh)
 {
-	int tich = 1;
-	tich = a * b;
-	return tich;
+	return canh*4;
 }
-float thuong(int a, int b)
+
+float dienTichHinhVuong(float canh)
 {
-	float thuong = 0;
-	if (b == 0)
-		cout << "Error";
-	else
-		thuong = float(a / b);
-	return thuong;
+	return canh*canh;
 }
-int cvdtHV(float chuvi,float dientich,int canh) {
-	cout << "nhap canh ";
-	cin >> canh;
-	chuvi = canh * 4;
-	dientich = canh * canh;
-	cout << "chu vi hinh vuong:" << chuvi << endl << "dien tich hinh vuong:" << dientich;
-}
-int cvdthHCN(float chuvi, float dientich, int a, int b) {
-	
-	cout << "nhap vao a:";
-	cin >> a;
-	cout << "nhap b:";
-	cin >> b;
-	chuvi = (a + b) * 2;
-	dientich = a * b;
-	cout << "chu vi hinh chu nhat:" << chuvi << endl << "dien tich hinh chu nhat:" << dientich;
-
-}
-int cvdtHT(float chuvi,float dientich, int r) {
-	
-	cout << "nhap vao r:";
-	chuvi = 2 * r*PI;
-	dientich = PI *r*r;
-
+float chuViHinhChuNhat(float dai, float rong)
+{
+	return (dai+rong)*2;
 }
 
+float dienTichHinhVuong(float dai, float rong)
+{
+	return dai*rong;
+}
+
+float chuViHinhTron(float bankinh)
+{
+	return 2*bankinh*3.14;
+}
+
+float dienTichHinhTron(float bankinh)
+{
+	return bankinh*bankinh*3.14;
+}
 int main()
 {
-	int a, b;
-	cout << "Nhap 2 so nguyen: ";
-	cin >> a >> b;
-	cout << a << "+" << b << "=" << tong(a, b) << endl;
-	cout << a << "-" << b << "=" << hieu(a, b) << endl;
-	cout << a << "*" << b << "=" << tich(a, b) << endl;
-	cout << a << "/" << b << "=" << thuong(a, b) << endl;
-
 	return 0;
 }
