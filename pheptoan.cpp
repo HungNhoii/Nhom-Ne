34ea6718cd64a1e4a1fcf70497536dc21df585ba
unknown <vu.hieuuu50@gmail.com>
2020-10-21 16:45:58 +0700

    update luyen tap cong lan 1

--- pheptoan.cpp
+++ pheptoan.cpp
@@ -1,32 +1,21 @@
 #include<iostream>
 using namespace std;
-int tong(int a, int b)
+void kiemTraLuyenTapCong()
 {
-	return a + b;
-}
-int hieu(int a, int b)
-{
-	return a - b;
-}
-int tich(int a, int b)
-{
-	return a * b;
-}
-float thuong(int a, int b)
-{
-	return a*1.0 / b;
+	int a, b, ketqua;
+	a = rand() % 100 + 1;
+	b = rand() % 100 + 1;
+	cout << a << " + " << b << " = ";
+	cout << "\nNhap ket qua phep trinh tren: ";
+	cin >> ketqua;
+	if (ketqua == a + b)
+		cout << "Ket qua dung!";
+	else
+		cout << "ket qua sai!";
 }
 int main()
 {
-	int a, b;
-	cout << "Nhap 2 so tu nhien: ";
-	cin >> a >> b;
-	cout << "Tong = " << tong(a, b) << endl;
-	cout << "Hieu = " << hieu(a, b) << endl;
-	cout << "Tich = " << tich(a, b) << endl;
-	if (b == 0)
-		cout << "so chia phai khac 0.";
-	else
-		cout << "Thuong = " << thuong(a, b) << endl;
+	
+	kiemTraLuyenTapCong();
 	return 0;
 }
